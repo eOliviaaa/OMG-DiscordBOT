5c54e631bb7c46c39dda340c01113277e6e43034
Ivylux <Ivylux.dev@gmail.com>
2023-08-22 14:10:48 +0200

    made POST request using axios

--- events/guildScheduledEventCreateAction.js
+++ events/guildScheduledEventCreateAction.js
@@ -1,15 +1,21 @@
 const { Events } = require('discord.js');
+const axios = require('axios').default;
 
 module.exports = {
     name: Events.GuildScheduledEventCreate,
     async execute(guildScheduledEventCreate) {
-        let event = {
+        axios.post('http://172.20.64.1:8080/event/create', {
             header: guildScheduledEventCreate.name,
             description: guildScheduledEventCreate.description,
             startTime: new Date(guildScheduledEventCreate.scheduledStartTimestamp).toISOString(),
             endTime: new Date(guildScheduledEventCreate.scheduledEndTimestamp).toISOString(),
             location: guildScheduledEventCreate.entityMetadata.location
-        }
-        console.log(event);
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 }
